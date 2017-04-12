4716b523abbd9a2995dca392d1ba0f72a6b1e69c
xiaomei <rh4466@163.com>
2017-04-12 10:52:50 +0800

    "分支添加新功能"

--- app/src/main/java/com/jj/selectaddreses/AddActivity.java
+++ app/src/main/java/com/jj/selectaddreses/AddActivity.java
@@ -3,12 +3,11 @@ package com.jj.selectaddreses;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String s = "new add";
+        setContentView(R.layout.activity_add);
     }
 }
